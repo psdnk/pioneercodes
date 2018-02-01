b37240c332dbfcd4c510fddf804ecb4bd467c090
prasad.naik <prasad.naik@redbus.com>
2018-02-02 01:34:02 +0530

    fib game commit

---
+++ FibonacciGame/src/App.java
@@ -0,0 +1,25 @@
+/**
+ * @author prasadnaik
+ *
+ */
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		
+		System.out.print("Row : ");
+		int row=s.nextInt();
+		System.out.println("Column :");
+		int col=s.nextInt();
+		
+		FibonacciGame fibonacciGame=new FibonacciGame(row, col);
+		fibonacciGame.start();
+		s.close();
+		
+		
+
+	}
+
+}
